23c2ba8cc932581a418670458ce62e2c4cd7c4cf
Nico-Bao <174931404+Nico-Bao@users.noreply.github.com>
2025-05-05 12:28:19 +0200

    Add setup_homebrew.zsh and run with Dotbot
    Install Homebrew
    Packages: httpie, bat
    Delete test stuff

---
+++ setup_homebrew.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Homebrew setup... >>>\n"
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Homebrew packages
+brew install httpie
+brew install bat
